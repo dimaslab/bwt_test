9c1bf0bf540c792133cb12884c5d43723114ad02
dimaslab <dimas_lab@mail.ru>
2016-02-25 20:19:33 +0200

    up

--- templates/save_user.php
+++ templates/save_user.php
@@ -36,6 +36,11 @@ $mail = trim($mail);
 
 $pass = md5($pass);
 
+$result = $mysqli->query("SELECT id FROM users WHERE login_name = '$login'")->fetch_assoc();
+if (!empty($result['id'])) {
+    exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
+}
+
 $stmt = $mysqli->prepare("INSERT INTO users(login_name,pass,f_name,l_name,sex,mail,b_date) VALUES (?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param('sssssss', $login, $pass, $f_name, $l_name, $sex, $mail, $b_date);
 $stmt->execute();
